7c5e172e1933015b88e42df8949560f363677821
khalid fifel <fifle.khalid99@gmail.com>
2023-03-14 16:27:25 +0100

    add the result component and it's view

---
+++ src/components/ResultQuiz.vue
@@ -0,0 +1,29 @@
+<template>
+  <article>
+    <h1>Result Quiz</h1>
+      Hello,
+        your score is : <h2><b class="text-blue">{{ (result * 100 / questionsLength).toFixed(3)}}% </b> from {{ questionsLength }} questions</h2>
+  </article>
+
+</template>
+
+<script>
+export default {
+  name: "ResultQuiz",
+  data(){
+
+  },
+  computed: {
+    result() {
+      return this.$store.getters.getCorrectAnswers;
+    },
+    questionsLength(){
+      return this.$store.state.questions.length;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ResultQuiz.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <ResultQuiz />
+    </div>
+</template>
+
+<script setup>
+  import  ResultQuiz  from '@/components/ResultQuiz.vue'
+  
+</script>
